f1549fa54b17c58db6d360c012066f737e316cce
Sunho12 <rhkrtjsgh1210@gmail.com>
2024-11-15 09:42:18 +0900

    [8주차] 미션 댓글 달기 API

--- src/main/java/claire/spring/web/dto/ReviewRequest.java
+++ src/main/java/claire/spring/web/dto/ReviewRequest.java
@@ -1,15 +1,24 @@
 package claire.spring.web.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 public class ReviewRequest {
 
     @Getter
     public static class ReviewDto{
+        @NotNull
         private Long userId;
+        @NotNull
         private Long missionId;
+        @NotNull
+        @Size(min = 1, max = 50)
         private String title;
+        @NotNull
+        @Size(min = 1, max = 200)
         private String content;
+        @NotNull
         private Float rating;
     }
 }
